61f11d01e18a157a43b187b28c8b7a71e0f37a97
Vasilios Tsapalos <littleprog@gmail.com>
2014-09-04 15:56:50 +0300

    Foreground added

--- Sepals/src/gr/sepals/gui/info/LogoActivity.java
+++ Sepals/src/gr/sepals/gui/info/LogoActivity.java
@@ -2,8 +2,6 @@ package gr.sepals.gui.info;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class LogoActivity extends Activity {
 
